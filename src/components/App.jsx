2eebaa871f0166ff2ab0f3ecf7818cfa6db0c339
bhuvaneshwaranR <bhuvaneshwaran1994@gmail.com>
2017-08-08 18:42:58 +0530

    initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+
+export default class App extends React.Component {
+  render() {
+    return (
+     <div style={{textAlign: 'center'}}>
+        <h1> Demo</h1>
+
+        <div>
+            
+            </div>
+      </div>);
+  }
+}
+
+/*
+import {Canvas,Circle, Image, Path, Text,IText} from 'react-fabricjs';
+<Canvas
+            ref="canvas"
+            width="1500"
+            height="1000"
+        >
+        
+ 
+            <Image
+                src="./src/images/black.jpg"
+                width={500}
+                height={500}
+                left={100}
+                top={100}
+            />
+             <Text
+            text="Hello World!"
+            left={300}
+            top={300}
+            fill="#000000"
+            fontFamily="Arial"
+          />
+ 
+ 
+         
+        </Canvas>  */
